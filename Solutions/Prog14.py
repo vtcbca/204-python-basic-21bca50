08a3766ed28fdb8973f77bd6cb2e8f20ea25b983
21bca50 <96609131+21bca50@users.noreply.github.com>
2022-03-27 13:20:39 +0530

    Update Prog14.py

--- Solutions/Prog14.py
+++ Solutions/Prog14.py
@@ -1,3 +1,12 @@
-i = int(input("Enter Value: "))
-for a in range(1,i+1):
-    print(" "*(i-a)+"* "*a)
+#Script to print reverse right triangle
+no=int(input("HOW many rows you want to print ?  "))
+a=2*no-2
+for i in range(0, no):
+    for j in range(0, a):
+        print(end=" ")
+    a = a - 2
+    for j in range(0, i+1):
+        print("* ",end="")
+    print("\r")
+ 
+© 2022 GitHub, Inc.
